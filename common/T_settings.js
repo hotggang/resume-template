e3532911bd224bb3f810530286006d79fc68b36b
gohsue-inflab <gohsue@inflab.com>
2024-01-07 20:38:37 +0900

    feat: 세팅 템플릿 추가

---
+++ common/T_settings.js
@@ -0,0 +1,23 @@
+// const html = String.raw;
+
+const T_settings = `
+	<section>
+		<button type="button">설정</button>
+		<aside>
+			<h2>기본 설정</h2>
+			<p>데이터를 관리합니다.</p>
+			<form class="settings__form">
+				<section class="settings__basic">
+					<input type="checkbox" id="settings__basic-name-toggle" />
+					<label for="settings__basic-name-toggle">name</label>
+					<input type="checkbox" id="settings__basic-job-toggle" />
+					<label for="settings__basic-job-toggle">job</label>
+					<input type="checkbox" id="settings__basic-email-toggle" />
+					<label for="settings__basic-email-toggle">email</label>
+				</section>
+			</form>
+		</aside>
+	</section>
+`;
+
+export default T_settings;
